345d6e2f81d864e8139b5d628df2e109ccc358bf
juanmera01 <Uo277406@uniovi.es>
2022-03-05 17:33:06 +0100

    hotfix variables de entorno BD

--- restapi/database.ts
+++ restapi/database.ts
@@ -1,13 +1,8 @@
-import { Console } from "console";
 
 
 //Conexion a la base de datos
 const mongoose = require('mongoose');
 
-const user = 'novendoagua';//process.env.DB_USER;
-const password = '0otLMkCQhT7YXtZB';//process.env.DB_PASSWORD;
-const uri = `mongodb+srv://${user}:${password}@cluster0.pebnb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })  //Hace que no se muestre info en la consola
+mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Base de datos conectada'))
-    .catch((e: any) => console.log(e))
+    .catch((e: any) => console.log(e));
--- restapi/server.ts
+++ restapi/server.ts
@@ -5,8 +5,11 @@ import promBundle from 'express-prom-bundle';
 import api from "./api"; 
 import 'reflect-metadata';
 
-
 // inicializacion y configuraciones
+const dotenv = require('dotenv');
+dotenv.config('.env');
+
+
 const app: Application = express();
 app.set('port', process.env.PORT || 5000);
 const databse = require('./database');
@@ -22,10 +25,10 @@ app.use(metricsMiddleware);
 app.use(cors(options));
 app.use(bp.json());
 
-app.use("/api", api)
+app.use("/api", api);
 
 app.listen(app.get('port'), ():void => {
     console.log('Restapi listening on '+ app.get('port'));
 }).on("error",(error:Error)=>{
     console.error('Error occured: ' + error.message);
-});
+}); 
