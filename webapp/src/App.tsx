0526b4f80682038e8173f7b412fe285dd83d3282
oscarlopezgonzalez <oscarnei@outlook.es>
2022-03-11 11:21:29 +0100

    Modifica la estructura del proyecto para poder juntarlo con el resto

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,44 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import Welcome from './components/Welcome';
-import UserList from './components/UserList';
-import Catalogo from './components/Catalogo';
-import  {getProducts, getUsers} from './api/api';
-import {User} from './shared/shareddtypes';
-import {Product} from './shared/shareddtypes';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import ConfirmacionEdad from './components/ConfirmacionEdad';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BarraNavegacion from './components/BarraNavegacion';
 
-function App(): JSX.Element {
 
-  const [products,setProducts] = useState<Product[]>([]);
-
-  const refreshProducts = async () => {
-    setProducts(await getProducts());
-  }
-
-  const [users,setUsers] = useState<User[]>([]);
-
-  const refreshUserList = async () => {
-    setUsers(await getUsers());
-  }
-
-  useEffect(()=>{
-    refreshProducts();
-    refreshUserList();
-  },[]);
-
+function App() {
   return (
-    <>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>       
-        <UserList users={users}/>
-        <Link href="https://github.com/Arquisoft/dede_es6a">Source code</Link>
-        <Catalogo products={products}/>
-      </Container>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<ConfirmacionEdad />} />
+        <Route path='/products' element={<BarraNavegacion />} />
+      </Routes>
+    </Router>
   );
 }
 
