f134a4eeb96dbd81364b8e54a1539db7862042db
David Maldonado <90855360+UO259893@users.noreply.github.com>
2022-03-11 13:37:18 +0100

    Update shareddtypes.ts

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -2,11 +2,3 @@ export type User = {
     name:string;
     email:string;
   }
-
-export type Product = {
-    nombre:string;
-    marca: string;
-    precio: number;
-    categoria: string;
-    descripcion: string;
-}
