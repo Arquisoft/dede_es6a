b4a32a2aa0dacbe0eaa7793a1255d1d15d876cb5
David Maldonado <90855360+UO259893@users.noreply.github.com>
2022-03-11 13:36:08 +0100

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,25 +1,15 @@
 import React, { useState, useEffect } from 'react';
-
-import Inicio from './components/Inicio';
-import Catalogo from './components/Catalogo';
-
-import  {getProducts, getUsers} from './api/api';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import EmailForm from './components/EmailForm';
+import Welcome from './components/Welcome';
+import UserList from './components/UserList';
+import  {getUsers} from './api/api';
 import {User} from './shared/shareddtypes';
-import {Product} from './shared/shareddtypes';
 import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 
 function App(): JSX.Element {
-
-  const [products,setProducts] = useState<Product[]>([]);
-
-  const refreshProducts = async () => {
-    setProducts(await getProducts());
-  }
 
   const [users,setUsers] = useState<User[]>([]);
 
@@ -28,21 +18,18 @@ function App(): JSX.Element {
   }
 
   useEffect(()=>{
-    refreshProducts();
     refreshUserList();
   },[]);
 
   return (
     <>
-   
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="inicio" element={<Inicio />} />
-        <Route path="catalogo" element={<Catalogo  />} />
-        
-      </Routes>
-      </BrowserRouter>
+      <Container maxWidth="sm">
+        <Welcome message="ASW students"/>
+        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
+        <EmailForm OnUserListChange={refreshUserList}/>        
+        <UserList users={users}/>
+        <Link href="https://github.com/Arquisoft/dede_es6b">Source code</Link>
+      </Container>
     </>
   );
 }
