2bda3f2fbf88b453b07f3fdbbd8b0f989e7bdf56
juanmera01 <Uo277406@uniovi.es>
2022-04-02 12:07:48 +0200

    mas validaciones

--- restapi/api.ts
+++ restapi/api.ts
@@ -85,15 +85,18 @@ api.get("/catalogo/:filter", async (req: Request, res: Response): Promise<Respon
 api.post("/login", async (req, res): Promise<Response>=> {
   var username = req.body.username;
   var password = req.body.password;
+  var usernameChecked:string;
 
   if(username == "")
     return res.status(401).send("Nombre de usuario no valido");
   const re = /^[a-zA-Z0-9_]*/;
   if(re.test(username) == false)
     return res.status(401).send("Nombre de usuario no valido");
+  else
+    usernameChecked = username;
 
   let hash = crypto.createHmac('sha256','abcdefg').update(password).digest('hex');
-  let user:UserType = await User.findOne({"username": username,'password': hash}) as UserType;
+  let user:UserType = await User.findOne({"username": usernameChecked,'password': hash}) as UserType;
   if(user != null){
     session.user = user.username;
     return res.status(200).send(user.username);
