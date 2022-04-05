db09eefe686236284fc286a611ee7128f2756a50
oscarlopezgonzalez <oscarnei@outlook.es>
2022-04-05 11:08:12 +0200

    Unifica todos los nombres de variable

--- restapi/database.ts
+++ restapi/database.ts
@@ -3,6 +3,6 @@
 //Conexion a la base de datos
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.RESTAPI_DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Base de datos conectada'))
     .catch((e: any) => console.log(e));
