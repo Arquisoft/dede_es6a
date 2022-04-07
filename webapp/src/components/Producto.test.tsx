3e678ae1def9891808ceda0d330c4a9e8a8df0c4
David Maldonado <90855360+UO259893@users.noreply.github.com>
2022-04-07 19:09:47 +0200

    Add files via upload

---
+++ webapp/src/components/Producto.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { Product } from '../shared/shareddtypes';
+import Producto from './Producto';
+
+
+
+
+
+test('Test renderizado producto', () => {
+
+
+  const productoParaTest: Product={id:"1", nombre:"ProductoPrueba", marca:"Nike", precio:10.0, categoria:"Testing", descripcion:"Producto de test para verificar el correcto funcionamiento de la app."};
+  render(<Producto props={productoParaTest} addToCarrito={()=>null}/> );
+
+  const nombre = screen.getByText("ProductoPrueba");
+  expect(nombre).toBeInTheDocument();
+  const precio = screen.getByText("10 €");
+  expect(precio).toBeInTheDocument();
+  const descripción = screen.getByText("Producto de test para verificar el correcto funcionamiento de la app.");
+  expect(descripción).toBeInTheDocument();
+ 
+});
+
+
