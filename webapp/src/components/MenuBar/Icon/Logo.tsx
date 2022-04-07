e8876daa3a402ec120e62a09d0d17beb94a9a2f7
David Maldonado <90855360+UO259893@users.noreply.github.com>
2022-04-07 19:07:41 +0200

    Delete webapp/src/components/MenuBar directory

--- webapp/src/components/MenuBar/Icon/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import LogoTienda from "./../../../assets/logo.png";
-
-import "./Logo.css";
-
-export default function Logo() {
-  return (
-    <img className="logo" src={LogoTienda} alt="Logo tienda"></img>
-  )
-}
