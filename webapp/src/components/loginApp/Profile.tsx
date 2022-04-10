43c18c2932a7222f8574c0eb04f42983ad2ea5c5
juanmera01 <Uo277406@uniovi.es>
2022-04-10 15:40:40 +0200

    datos de usuario en ventana de perfil

--- webapp/src/components/loginApp/Profile.tsx
+++ webapp/src/components/loginApp/Profile.tsx
@@ -1,31 +1,65 @@
 import BarraNavegacion from '../BarraNavegacion';
 import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
-import {getOrdersByClientLogged, isLogged} from '../../api/api';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import {getOrdersByClientLogged, isLogged, getUserLoggeed, logout} from '../../api/api';
 import { useState, useEffect } from 'react';
-import {isLoggedType, OrderFromDB} from '../../shared/shareddtypes';
+import {isLoggedType, OrderFromDB, User} from '../../shared/shareddtypes';
 import ErrorPage from '../ErrorPage';
+import toast from 'react-hot-toast';
+import './profile.css';
 
 export default function Profile() {
 
     const [log,setIsLogged] = useState<isLoggedType>();
     const [orders,setOrders] = useState<OrderFromDB[]>([]);
+    const [user,setUser] = useState<User[]>([]);
+    const refreshUser = async () => {
+        setUser(await getUserLoggeed());
+    }
     const refreshOrders = async () => {
         setOrders(await getOrdersByClientLogged());
     }
     const refreshIsLogged = async () => {
         setIsLogged(await isLogged());
     }
-    useEffect(()=>{ refreshOrders(); refreshIsLogged(); }, []);
+    useEffect(()=>{ refreshOrders(); refreshIsLogged(); refreshUser(); }, []);
+
+    const logoutButton = async () => {
+        await logout();
+        toast.success('Sesión cerrada', {duration: 800});
+        setTimeout(() => {
+            (document.getElementById("navigate") as HTMLAnchorElement).click();
+        }, 1000);
+    }
+
+    const setUserData = () => {
+        if(user[0]){
+            return(
+                <>
+                    <ListGroup.Item>{user[0].username}</ListGroup.Item>
+                    <ListGroup.Item>{user[0].email}</ListGroup.Item>
+                </>
+            );
+        }
+    }
 
     if(log?.logged){
         return(
             <>
-                <BarraNavegacion />
+                <div id='userdata'>
+                    <BarraNavegacion />
+                    <h3>Usuario</h3>
+                    <ListGroup>
+                        {setUserData()}
+                    </ListGroup>
+                    <Button id='logout' variant="primary" onClick={logoutButton}>Cerrar sesión</Button>{' '}
+                    <a href='/catalogo' id='navigate' hidden></a>
+                </div>
                 <h3>Pedidos realizados</h3>
                 <Accordion defaultActiveKey="0" flush>
                     
-
                     {orders.map((order)=>{   
                         return(
                             <Accordion.Item eventKey={orders.indexOf(order)+""}>
