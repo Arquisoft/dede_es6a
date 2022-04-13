91b32841390e154ff49409f921b8a97497b5bce4
juanmera01 <Uo277406@uniovi.es>
2022-04-13 23:17:20 +0200

    intento de deploy 4

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -24,15 +24,15 @@ const App = () => {
       <HashRouter>
       <Routes>
         <Route path="/" element={<ConfirmacionEdad />} />
-        <Route path="catalogo" element={<Catalogo />} />
-        <Route path="products/add" element={<AddProducts />} />
-        <Route path="users/list" element={<ListUsers />} />
-        <Route path="carrito" element={<Carrito />} />
-        <Route path="pedido" element={<DatosPedido />} />
-        <Route path="pago" element={<ConfirmacionPago />} />
-        <Route path="aboutus" element={<AboutUs />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/products/add" element={<AddProducts />} />
+        <Route path="/users/list" element={<ListUsers />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/pedido" element={<DatosPedido />} />
+        <Route path="/pago" element={<ConfirmacionPago />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Toaster
         position={"bottom-left"}
