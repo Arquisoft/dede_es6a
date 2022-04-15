be7ca363e17b702a8f9b1bac14533452b080a04f
juanmera01 <Uo277406@uniovi.es>
2022-04-15 10:18:26 +0200

    intento de arreglar navegacion en el despliegue

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,7 @@ import {
   HashRouter,
   Routes,
   Route,
+  BrowserRouter,
 } from "react-router-dom";
 import './App.css';
 import AddProducts from './components/products/addProducts/AddProducts';
@@ -21,24 +22,24 @@ const App = () => {
 
   return(
     <>
-      <HashRouter>
-      <Routes>
-        <Route path="/" element={<ConfirmacionEdad />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/products/add" element={<AddProducts />} />
-        <Route path="/users/list" element={<ListUsers />} />
-        <Route path="/carrito" element={<Carrito />} />
-        <Route path="/pedido" element={<DatosPedido />} />
-        <Route path="/pago" element={<ConfirmacionPago />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ConfirmacionEdad />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/products/add" element={<AddProducts />} />
+          <Route path="/users/list" element={<ListUsers />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/pedido" element={<DatosPedido />} />
+          <Route path="/pago" element={<ConfirmacionPago />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
       <Toaster
         position={"bottom-left"}
         reverseOrder={false}
       />
-      </HashRouter>
     </>
   );
 };
